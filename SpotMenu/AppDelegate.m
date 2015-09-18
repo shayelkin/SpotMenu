6ffb7c127ac3bb70d71f5b7b0f45dfebb1a75492
Shay Elkin <2046772+shayelkin@users.noreply.github>
2015-09-17 23:07:44 -0700

    First working version

--- SpotMenu/AppDelegate.m
+++ SpotMenu/AppDelegate.m
@@ -10,17 +10,71 @@
 
 @interface AppDelegate ()
 
-@property (weak) IBOutlet NSWindow *window;
+@property (strong, nonatomic) NSStatusItem *statusItem;
+
 @end
 
 @implementation AppDelegate
 
+
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
+    _statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
+    [_statusItem setToolTip:NSLocalizedString(@"⌘-Click to Quit", nil)];
+    [_statusItem setAction:@selector(statusItemClicked:)];
+    [_statusItem setTitle:@"SpotMenu"];
+    [_statusItem setEnabled:YES];
+    
+    [NSTimer scheduledTimerWithTimeInterval:1.0f target:self selector:@selector(updateStatusItemTitle:) userInfo:nil repeats:YES];
+    [self updateStatusItemTitle:nil];
 }
 
-- (void)applicationWillTerminate:(NSNotification *)aNotification {
-    // Insert code here to tear down your application
+- (void)updateStatusItemTitle:(id)sender
+{
+    NSString* trackName = [self getCurrentPlayingTrack];
+    NSLog(@"trackName == %@", trackName);
+    
+    if (trackName) {
+        [_statusItem setTitle: trackName];
+    }
+}
+
+- (void)statusItemClicked:(id)sender
+{
+    NSEvent *event = [NSApp currentEvent];
+    if ([event modifierFlags] & NSCommandKeyMask) {
+        [[NSApplication sharedApplication] terminate:self];
+    }
+}
+
+- (NSString*)getCurrentPlayingTrack
+{
+    NSString *trackName = NULL;
+    NSDictionary* errorDict;
+    NSAppleEventDescriptor* returnDescriptor = NULL;
+    
+    NSAppleScript* scriptObject = [[NSAppleScript alloc] initWithSource:
+                                    @"if application \"Spotify\" is running then\n"
+                                    " tell application \"Spotify\"\n"
+                                    "  if player state is playing then\n"
+                                    "   set theTrack to name of the current track\n"
+                                    "   set theArtist to artist of the current track\n"
+                                    "   return ({theTrack, \" - \", theArtist} as string)\n"
+                                    "  end if\n"
+                                    " end tell\n"
+                                    "end if"];
+    
+    returnDescriptor = [scriptObject executeAndReturnError: &errorDict];
+    
+    NSLog(@"returnDescriptor == %@", returnDescriptor);
+    if (returnDescriptor != NULL) {
+        // successful execution
+        if (kAENullEvent != [returnDescriptor descriptorType]) {
+            // assume AppleScript result is UTF-8
+            trackName = [[NSString alloc] initWithData:[returnDescriptor data] encoding:NSUTF8StringEncoding];
+        }
+    }
+    
+    return trackName;
 }
 
 @end
